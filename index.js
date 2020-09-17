78521f2e96f9f1d11012dcc97f597f6f6cc62956
Emma Bostian <bostian@spotify.com>
2020-09-17 19:04:41 +0200

    LOLOL

---
+++ index.js
@@ -0,0 +1,35 @@
+const playButton = document.querySelector("#play");
+const closeButton = document.querySelector("#close");
+const firstInput = document.querySelector("input");
+const body = document.querySelector("body");
+const modal = document.querySelector("#modal");
+let count = 0;
+
+playButton.addEventListener("click", handlePlayClick);
+
+function handlePlayClick() {
+  if (count === 0) {
+    playButton.textContent = "Are you sure?";
+    count++;
+  } else if (count === 1) {
+    playButton.textContent = "LOL K";
+    const audio = new Audio("lol.mp3");
+    audio.play();
+  }
+}
+
+window.setTimeout(() => {
+  modal.style.opacity = 1;
+}, 2000);
+
+closeButton.addEventListener("click", () => {
+  window.setTimeout(() => {
+    modal.style.opacity = 0;
+  }, 1000);
+});
+
+firstInput.addEventListener("keydown", () => {
+  window.setTimeout(() => {
+    firstInput.value = "";
+  }, 500);
+});
